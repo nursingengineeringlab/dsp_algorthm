a513ae6d22ec2583007691c6f62177bac42966b3
Samridh Tuteja <92537751+samridh17@users.noreply.github.com>
2023-06-02 12:38:04 -0400

    Add files via upload

--- feature_extraction.c
+++ feature_extraction.c
@@ -7,21 +7,21 @@
 #define MA_filter_size 50
 #define emg_signal_size 64
 
-int16_t emg_value_raw;                           
-int16_t emg_mean_absolute_value;
-int16_t emg_integrated_absolute;                                                                                                    
-int16_t emg_integrated;                                                              
+int32_t emg_value_raw;                           
+int32_t emg_mean_absolute_value;                                                                                                
+int32_t emg_integrated;                                                              
 int32_t emg_variance;
-int32_t emg_ssi;
-int32_t emg_ssi_squared;
-int32_t emg_root_mean_value;
-int16_t emg_root_mean_square;
+int64_t emg_ssi;
+int64_t emg_ssi_squared;
+int64_t emg_root_mean_value;
+int64_t emg_root_mean_square;
 
 uint8_t emg_array_raw_index;
 uint8_t emg_array_out_index;
 
-int16_t emg_array_raw[MA_filter_size];
-int16_t emg_array_out[emg_signal_size];
+int32_t emg_array_raw[MA_filter_size];
+int32_t emg_array_out[emg_signal_size];
+int8_t test_array[200];
 
 
 int main(void){
@@ -32,9 +32,17 @@ int main(void){
     uint8_t i;
     uint8_t j;
     uint8_t k;
+    uint8_t s;
+    uint8_t t=0;
+    uint8_t z;
 
-    for(;;){
-        emg_value_raw = rand();
+    for(z=0; z < 200; z++){
+        test_array[z] = -z;
+    }
+
+    for(s=0; s<200; s++){
+        emg_value_raw = test_array[t];
+        t++;
         emg_array_raw[emg_array_raw_index] = emg_value_raw;
 
         printf("EMG: %d\n", emg_value_raw);
@@ -46,7 +54,7 @@ int main(void){
             emg_array_raw_index = 0;
         }
 
-        for(i = 0; i < MA_filter_size - 1; i++){
+        for(i = 0; i < MA_filter_size; i++){
             MA_sum += emg_array_raw[i];
         }
         emg_array_out[emg_array_out_index] = MA_sum / MA_filter_size;
@@ -59,8 +67,10 @@ int main(void){
             emg_array_out_index = 0;
         }
         for(j = 0; j < emg_signal_size; j++){
-            emg_integrated_absolute = labs(emg_array_out[j]);
-            emg_integrated += emg_integrated_absolute;
+            if(emg_array_out[j] < 0){
+                emg_array_out[j] = emg_array_out[j] * (-1);
+            }
+            emg_integrated += emg_array_out[j];
         }
 
         emg_mean_absolute_value = emg_integrated / emg_signal_size;
@@ -79,5 +89,15 @@ int main(void){
         printf("SSI: %d\n", emg_ssi);
         printf("EMG Variance: %d\n", emg_variance);
         printf("RMS: %d\n", emg_root_mean_square);
+
+
+        emg_mean_absolute_value = 0;                                                                                                  
+        emg_integrated = 0;                                                              
+        emg_variance = 0;
+        emg_ssi = 0;
+        emg_ssi_squared = 0;
+        emg_root_mean_value = 0;
+        emg_root_mean_square = 0;
+
     }
 }
